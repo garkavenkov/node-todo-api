bcb43cdfbb28d473b4daf2db0c909f10c32da847
Maksim Garkavenkov <maksim.garkavenkov@gmail.com>
2016-12-22 21:16:46 +0200

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -2,6 +2,7 @@ const {
     SHA256
 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
@@ -29,12 +30,26 @@ const jwt = require('jsonwebtoken');
 //     console.log('Data was change. Do not trust.');
 // }
 
-var data = {
-    id: 10
-}
+// var data = {
+//     id: 10
+// }
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded ', decoded);
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var password = '123abc!';
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded ', decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
+
+var hashedPassword = '$2a$10$/7YbGIXKQTLAVIYB0YXIV.fywfAi5vc3L//oPv4rsywA2/yBIszD2';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
